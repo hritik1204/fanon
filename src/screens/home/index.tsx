af76a414ffcd92dc0621df0b54f70498cf9d5170
Your Personal Name <your_personal_email@example.com>
2025-09-25 02:02:09 +0530

    Update StatusBar style to 'light' for improved visibility and set HomeScreen background color to dark theme for better aesthetics.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/src/components/ui/header";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeBody } from "./components";
 import { StyleSheet } from "react-native";
+import { Colors } from "@/src/constants/theme";
 
 const HomeScreen = () => {
   return (
@@ -18,5 +19,6 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Colors.dark.black
   },
 });
